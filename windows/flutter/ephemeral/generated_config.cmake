760e5d249422e3bd1becd899b9d84bcbfc8e514b
Nikita <154605979+SalomandraC@users.noreply.github.com>
2025-02-12 21:33:13 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\podko\\Desktop\\FCAPP\\fcap" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "PROJECT_DIR=C:\\Users\\podko\\Desktop\\FCAPP\\fcap"
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\podko\\Desktop\\FCAPP\\fcap\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\podko\\Desktop\\FCAPP\\fcap"
+  "FLUTTER_TARGET=C:\\Users\\podko\\Desktop\\FCAPP\\fcap\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\podko\\Desktop\\FCAPP\\fcap\\.dart_tool\\package_config.json"
+)
